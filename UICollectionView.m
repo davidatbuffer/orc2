efdce89bc1afca9ce041ab7d96f1e2147b9df337
davidatbuffer <davidatbuffer@users.noreply.github.com>
2016-02-10 18:04:39 +0200

    Update UICollectionView.m

--- UICollectionView.m
+++ UICollectionView.m
@@ -5,9 +5,6 @@ NSArray *collectionImages;
 //        Add this code inside the ViewDidLoad method in the class ‘CollectionViewClass’.
 
 collectionImages = [NSArray arrayWithObjects:@"image1.png",@"image2.png",@"image3.png",@"image4.png",@"image5.png",@"image6.png",@"image7.png",@"image8.png",@"image9.png",@"image10.png",@"image11.png", nil];
-1
-	
-collectionImages = [NSArray arrayWithObjects:@"image1.png",@"image2.png",@"image3.png",@"image4.png",@"image5.png",@"image6.png",@"image7.png",@"image8.png",@"image9.png",@"image10.png",@"image11.png", nil];
 
  //                      At last we need to implement the UICollectionView delegate methods. Now add the below methods inside the CollectionViewClass.
 
